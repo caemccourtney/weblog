7ce8f47194ff47c9b88e6fa05f56a8e0f48beb30
Carlos Mc Courtney <carlos.edu.mcc@gmail.com>
2018-10-18 11:23:40 +0000

    Add article show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,19 @@ class ArticlesController < ApplicationController
     end
     
     def create
+        @article = Article.new(article_params)
     
-    render plain: params[:article].inspect
-    
+        @article.save
+        redirect_to @article
     end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+    
 end
